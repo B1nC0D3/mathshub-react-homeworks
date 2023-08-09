cd1398cf093e55d7fd63f2b83dc403deb9367c3b
Kirill <b1nc0d3.kka@gmail.com>
2023-08-09 06:24:17 +0300

    feature: added profile components

---
+++ cv-landing/src/components/profile/ProfilePicture.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import profile_pic from "../../imgs/profile_pic.jpg"
+function ProfilePicture() {
+    return(
+        <div>
+            <img src={profile_pic} alt="profile pic"/>
+        </div>
+    )
+}
+
+export default ProfilePicture
