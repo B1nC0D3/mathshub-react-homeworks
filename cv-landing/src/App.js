d4a5ec7d77e0612a1ac90badc73e2429ec1f98ee
Kirill <b1nc0d3.kka@gmail.com>
2023-08-09 06:23:44 +0300

    init: started react proj

---
+++ cv-landing/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Profile from "./components/Profile";
+function App() {
+    return (
+        <main>
+            <Profile/>
+        </main>
+    )
+}
+
+export default App;
